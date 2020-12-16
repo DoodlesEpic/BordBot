611d0094f7a30ac5a4a22e0d40d3927f64de5d19
Eduardo <eduardomoraes84@yahoo.com.br>
2020-12-16 02:12:46 +0000

    Exibir corretamente usuário inicializado

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-  print(f"BordBot inicializado como {client}");
+  print(f"BordBot inicializado como {client.user}");
 
 @client.event
 async def on_message(message):
