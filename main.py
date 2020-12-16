f90ead7c1c4c4532985761d7b62f31c2e9aa848b
Eduardo <eduardomoraes84@yahoo.com.br>
2020-12-16 02:06:27 +0000

    on_message event

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ client = discord.Client()
 @client.event
 async def on_ready():
   print(f"BordBot inicializado como {client}");
+
+@client.event
+async def on_message(message):
+  # Não responder a mensagens enviadas pelo própio bot
+  if message.author == client.user:
+    return
+
+  if message.content.startswith("bord"):
+    await message.channel.send("TÁ!!!")
